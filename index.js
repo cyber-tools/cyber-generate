7ed2e06b0473ad3703386c7d49bc380ff68b0b32
shicheng.yu <shicheng.yu@fawaimanongdeMacBook-Pro.local>
2020-07-03 17:43:17 +0800

    2020年07月03日17点43分17秒的快捷提交

---
+++ index.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+require("module-alias/register");
+const fs = require("fs");
+const path = require("path");
+
+const postGenerate = require("@/actions/postGenerate");
+const autoGenerate = require("@/actions/autoGenerate");
+const autoComplate = require("@/actions/autoComplate");
+
+postGenerate();
+fs.watch(path.join(process.cwd(), "./src/"), { recursive: true }, (eventType, filename) => {
+  // 根据文件后缀名填充文件内容
+  autoComplate(eventType, filename);
+  // 自动生成runtime代码
+  autoGenerate(eventType, filename);
+});
